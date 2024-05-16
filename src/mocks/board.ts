15311d517233292310ce1fba4316cf1f52ce3e53
Manoel Pereira dos Santos <manoelps@live.com>
2024-05-15 22:52:01 -0300

    mocado o tabuleiro

---
+++ src/mocks/board.ts
@@ -0,0 +1,38 @@
+export const board = [
+  {
+    id: 1,
+    owner: ''
+  },
+  {
+    id: 2,
+    owner: ''
+  },
+  {
+    id: 3,
+    owner: ''
+  },
+  {
+    id: 4,
+    owner: ''
+  },
+  {
+    id: 5,
+    owner: ''
+  },
+  {
+    id: 6,
+    owner: ''
+  },
+  {
+    id: 7,
+    owner: ''
+  },
+  {
+    id: 8,
+    owner: ''
+  },
+  {
+    id: 9,
+    owner: ''
+  }
+];
